f702a3400d102ad375070ba3e663b9acc9865b10
Jorge <manzoj@oregonstate.edu>
2019-01-22 15:34:32 -0800

    Implemented setLocatoin() and two constructors and a dup function

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -9,21 +9,43 @@ public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares = new ArrayList<>();
 
-	@JsonProperty private String kind;
+	@JsonProperty private int size;
 
 	public Ship(String shipType) {
-		kind = shipType;
+		if("MINESWEEPER".equals(shipType)) size = 2;
+		else if("DESTROYER".equals(shipType)) size = 3;
+		else if("BATTLESHIP".equals(shipType)) size = 4;
+		else throw new IllegalArgumentException("shipType");
+
+	}
+
+	public Ship(int size) {
+		this.size = size;
 	}
 
 	public List<Square> getOccupiedSquares() {
 		return occupiedSquares;
 	}
 
-	public boolean setLocation(int x, char y) {
-		return false;
+	public boolean setLocation(int x, char y, boolean isVertical) {
+		if(isVertical){
+			if(x < 1 || x > 11 - size || y < 'A' || y > 'J') return false;
+		}
+
+		else {
+			if(x < 1 || x > 10 || y < 'A' || y > 'K' - size) return false;
+		}
+
+		for(var i = 0; i < size; i++){
+
+			Square square = new Square(isVertical ? x + i : x, isVertical ? y : (char)(y + i));
+			occupiedSquares.add(square);
+		}
+		return true;
+
 	}
 
 	public Ship dup() {
-		return new Ship(kind);
+		return new Ship(size);
 	}
 }
