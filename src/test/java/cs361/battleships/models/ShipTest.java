0c68bfbfa9ec0c92f764dd911cafdb4c8f160f47
Jorge <manzoj@oregonstate.edu>
2019-01-22 16:16:37 -0800

    Created two tests for the Ship class methods

--- src/test/java/cs361/battleships/models/ShipTest.java
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -1,4 +1,19 @@
 package cs361.battleships.models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class ShipTest {
+    @Test
+    public void testSetLocation() {
+        Ship ship = new Ship(2);
+        assert(ship.setLocation(4,'B', false));
+    }
+
+    @Test
+    public void testInvalid() {
+        Ship ship = new Ship(2);
+        assertFalse(ship.setLocation(20,'O', false));
+    }
 }
